af7b5381ac3cd4ffc67f02cb8e73343fdc660b9a
javitocor <56809963+javitocor@users.noreply.github.com>
2020-07-14 06:11:25 +0200

    Add sunrise and sunset time

--- src/index.js
+++ src/index.js
@@ -14,3 +14,4 @@ const home = document.getElementById('home');
 home.addEventListener('click', ()=>{ location.reload(); });
 
 
+
--- src/js/dom.js
+++ src/js/dom.js
@@ -14,16 +14,18 @@ const dom = function () {
 
     function fillCard(data) {
         helpers().addInnerText('title', data['name']);
-        helpers().addInnerText('temp', '<i class="fas fa-thermometer-half text-warning my-2"></i> ' + data['main']['temp'] + ' Celsius');
-        helpers().addInnerText('feel', '<i class="fas fa-meteor text-warning my-2"></i> ' + data['main']['feels_like'] + ' Celsius');
+        helpers().addInnerText('temp', '<i class="fas fa-thermometer-half text-warning my-2"></i> Temp: ' + data['main']['temp'] + ' Celsius');
+        helpers().addInnerText('feel', '<i class="fas fa-meteor text-warning my-2"></i> Feeling: ' + data['main']['feels_like'] + ' Celsius');
         helpers().addInnerText('desc', '<i class="fas fa-cloud-sun-rain text-warning my-2"></i> ' + data['weather'][0]['main']);
-        helpers().addInnerText('pressure', '<i class="fas fa-compress-arrows-alt text-warning my-2"></i> ' + data['main']['pressure'] + ' hPa');
-        helpers().addInnerText('humidity', '<i class="fas fa-percent text-warning my-2"></i> ' + data['main']['humidity'] + '%');
-        helpers().addInnerText('minTemp','<i class="fas fa-temperature-low text-warning my-2"></i> ' + data['main']['temp_min'] + ' Celsius');
-        helpers().addInnerText('maxTemp','<i class="fas fa-temperature-high text-warning my-2"></i>' + data['main']['temp_max'] + ' Celsius');
+        helpers().addInnerText('pressure', '<i class="fas fa-compress-arrows-alt text-warning my-2"></i> Pressure: ' + data['main']['pressure'] + ' hPa');
+        helpers().addInnerText('humidity', '<i class="fas fa-percent text-warning my-2"></i> Humidity: ' + data['main']['humidity'] + '%');
+        helpers().addInnerText('minTemp','<i class="fas fa-temperature-low text-warning my-2"></i> Min: ' + data['main']['temp_min'] + ' Celsius');
+        helpers().addInnerText('maxTemp','<i class="fas fa-temperature-high text-warning my-2"></i> Max: ' + data['main']['temp_max'] + ' Celsius');
         helpers().addInnerText('wind','<i class="fas fa-wind text-warning my-2"></i> ' + data['wind']['speed'] + ' meter/sec');
         helpers().addInnerText('windDir','<i class="fas fa-compass text-warning my-2"></i> ' + data['wind']['deg'] + ' degrees');
-        helpers().addInnerText('clouds','<i class="fas fa-cloud text-warning my-2"></i> ' + data['clouds']['all'] + '%');
+        helpers().addInnerText('clouds','<i class="fas fa-cloud text-warning my-2"></i> Clouds: ' + data['clouds']['all'] + '%');
+        helpers().addInnerText('sunrise','<i class="fas fa-sun text-warning my-2"></i> ' + `Sunrise: ${new Date((data.sys.sunrise + data.timezone) * 1000).toLocaleTimeString().slice(-10, -6)} AM`);
+        helpers().addInnerText('sunset','<i class="fas fa-sun text-warning my-2"></i> ' + `Sunset: ${new Date((data.sys.sunset + data.timezone) * 1000).toLocaleTimeString().slice(-10, -6)} PM`);
     }
 
     function imageSwitch(data) {
